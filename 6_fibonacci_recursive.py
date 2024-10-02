b5822908399b6e22409db069773c9046391dd585
Peter Thibodeau <158618486+peter-thibodeau@users.noreply.github.com>
2024-10-02 11:07:45 -0400

    Add files via upload

---
+++ 6_fibonacci_recursive.py
@@ -0,0 +1,29 @@
+print('GET FIBONACCI USING RECURSION')
+def fibonacci(n):
+    if n <= 0:
+        print("Error! positive whole numbers greater than zero only.")
+    elif n == 1: # the fibonacci number for 1 is 0 (no formula necessary)
+        return 0
+    # Second fibonacci number is 1
+    elif n == 2: # the fibonacci number for 2 is 1 (no formula necessary)
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# test cases
+x = 1
+f = fibonacci(x)
+print(f"The factorial for the number {x} is {f}")
+
+x = 10
+f = fibonacci(x)
+print(f"The factorial for the number {x} is {f}")
+
+x = 5
+f = fibonacci(x)
+print(f"The factorial for the number {x} is {f}")
+
+x = 20
+f = fibonacci(x)
+print(f"The factorial for the number {x} is {f}")
+
