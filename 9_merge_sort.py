df5c488ed354c8ed4b905d9b8a414fee2768d8d2
Peter Thibodeau <158618486+peter-thibodeau@users.noreply.github.com>
2024-10-16 13:37:03 -0400

    Update 9_merge_sort.py

--- 9_merge_sort.py
+++ 9_merge_sort.py
@@ -1,9 +1,10 @@
 """
 The Merge Sort algorithm works this way:
-    1.	Subdivide the array two around the pivot, each sub-array should be half as big as the original.
+    1.	Subdivide the array at the pivot, each sub-array should be half as big as the original.
     2.	Continue to subdivide if the current sub-array has more than one element.
-    3.	Sort and merge two sub-arrays together.
-    4.	Repeat until there are no more sub-arrays.
+    3.	Sort the sub-arrays.
+    4.  Merge two sub-arrays together.
+    5.	Repeat until there are no more sub-arrays.
 """
 
 def mergeSort(lst):
@@ -15,16 +16,17 @@ def mergeSort(lst):
     left = lst[:mid]
     right = lst[mid:]
 
-    # continue by using recursion
+    # continue with recursion
     left_sort = mergeSort(left)
     right_sort = mergeSort(right)
 
-    return merge(left_sort, right_sort) # get them sorted
+    return actually_do_it(left_sort, right_sort) # do the actual sorting and merging
 
-def merge(left, right): # sorting
+def actually_do_it(left, right):
     result = []
     i = j = 0
 
+    # sorting
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             result.append(left[i])
@@ -33,14 +35,14 @@ def merge(left, right): # sorting
             result.append(right[j])
             j += 1
 
-    result.extend(left[i:]) # add smaller items before pivot
-    result.extend(right[j:]) # add larger items after pivot
+    # merging
+    result.extend(left[i:])
+    result.extend(right[j:])
 
     return result
 
 if __name__ == "__main__":
     lst = ['banana', 'apple', 'raisin', 'cherry']
-    merged_lst = mergeSort(lst)
-    print("Sorted lstay:", merged_lst)
-
+    print("Original list: ", lst)
+    print("Sorted list: ", mergeSort(lst))
 
