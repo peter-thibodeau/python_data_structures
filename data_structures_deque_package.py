5977a8ee4a7ec19ccb4bcdfb168e68661cd38334
Peter Thibodeau <158618486+peter-thibodeau@users.noreply.github.com>
2024-10-25 10:58:27 -0400

    Add files via upload

---
+++ data_structures_deque_package.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class DequeQueue:
+    def __init__(self):
+        self.queue = deque()
+
+    def enqueue(self, item):
+        self.queue.append(item) # add item to end of queue
+
+    def dequeue(self):
+        if not self.is_empty(): # call is_empty method
+            return self.queue.popleft() # return first item in list
+        else:
+            return "Queue is empty"
+
+    def is_empty(self): # see if queue is empty
+        return len(self.queue) == 0
+
+
+    def size(self):
+        return len(self.queue)  # return size of queue
+
+# create an instance of Queue
+q = DequeQueue()
+
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+
+# test cases
+print(f"The first item from the queue is {q.dequeue()}")
+print(f"Is the queue empty? {q.is_empty()}")
+print(f"Number of items remaining in the queue is {q.size()}\n")
+
+print(f"The next item from the queue is {q.dequeue()}")
+print(f"Is the queue empty? {q.is_empty()}")
+print(f"Number of items remaining in the queue is {q.size()}\n")
+
+print(f"The next item from the queue is {q.dequeue()}")
+print(f"Is the queue empty? {q.is_empty()}")
+print(f"Number of items remaining in the queue is {q.size()}")
